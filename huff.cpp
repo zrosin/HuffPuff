b7dc2b2b52fdc728e8f7821e46f66fefa59d3a89
zrosin <77403498+zrosin@users.noreply.github.com>
2021-11-08 17:47:16 -0600

    remove using std because conflicts

--- huff.cpp
+++ huff.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-using namespace std;
 
 struct TreeNode {
-	int glyph;
+	int glyph = -1;
+	int frequency = 0;
 	int leftptr = -1;
 	int rightptr = -1;
-};
-struct Glyph {
-	int glyph;
-	int freq;
+
+	bool operator<(TreeNode& other) {
+		return (this->frequency < other.frequency);
+	}
+	bool operator>(TreeNode& other) {
+		return (this->frequency > other.frequency);
+	}
+	bool operator==(TreeNode& other) {
+		return (this->frequency == other.frequency);
+	}
 };
 
-string fileName;
+std::string fileName;
 
 //Array is built right to left, so this is ptr at what should be the "0" element.
 int huffStart = 512;
 TreeNode huffmanTable[513];
 
 int freqEnd = 256;
-//inits every element to 0
-int frequencyTable[257] = { 0 };;
+TreeNode frequencyTable[257] = { 0 };
 
-ifstream fin;
+std::ifstream fin;
 const int bufferSize = 16;
 int bufferRemainder = 0;
 
-void checkFin(ifstream& fin) {
+void checkFin(std::ifstream& fin) {
 	//Check file opened
 	if (!fin.is_open()) {
 		cout << "Error reading " << fileName << " please check filename." << endl;
@@ -36,29 +41,34 @@ void checkFin(ifstream& fin) {
 }
 
 int main() {
-	cin >> fileName;
+	std::cin >> fileName;
 
 	//Read entire file into a buffer variable;
 	//While doing so add the frequencies to frequencyList;
-	fin.open(fileName, ios::hex);
+	fin.open(fileName, std::ios::hex);
 	
 	//comment this out for final build.
 	checkFin(fin);
 
 	//get file size
-	fin.seekg(0, ios::end);
+	fin.seekg(0, std::ios::end);
 	int size = fin.tellg();
 	fin.seekg(0);
 	bufferRemainder = size % bufferSize;
 
-	vector<unsigned char [bufferSize]> fileContent((size / bufferSize) + (bool)bufferRemainder);
+	std::vector<unsigned char [bufferSize]> fileContent((size / bufferSize) + (bool)bufferRemainder);
+
+	for (int i = 0; i < fileContent.size(); ++i)
+	{
+		frequencyTable[i].glyph = i;
+	}
 
 	unsigned char buffer[bufferSize];
-	for (int i = 0; i < (size / bufferSize); i++) {
+	for (int i = 0; i < (size / bufferSize); ++i) {
 		fin.read((char*)buffer, bufferSize);
 
-		for (int j = 0; j < bufferSize; j++) {
-			frequencyTable[buffer[i]] += 1;
+		for (int j = 0; j < bufferSize; ++j) {
+			frequencyTable[buffer[i]].frequency += 1;
 		}
 
 		fileContent.push_back(buffer);
@@ -69,18 +79,23 @@ int main() {
 		fin.read((char*)buffer, bufferSize);
 
 		for (int i = 0; i < bufferRemainder; i++) {
-			frequencyTable[buffer[i]] += 1;
+			frequencyTable[buffer[i]].frequency += 1;
 		}
 
 		fileContent.push_back(buffer);
 	}
 
 	//condense freqtable and set freqend variable;
+	for (int i = 0; i < 257 && frequencyTable[i].frequency != 0; ++i) {
+
+	}
 
 	//Quicksort
 
 	//create huffman table
 
 	//encode 
+
+
 
 }
